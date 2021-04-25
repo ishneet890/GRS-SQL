33c27d86250d34d596f54d1644cebf2eb932776d
Ishneet Sethi <63774944+ishneet890@users.noreply.github.com>
2021-04-26 03:57:46 +0530

    Create municipal.sql

---
+++ sql/municipal.sql
@@ -0,0 +1,15 @@
+CREATE TABLE municipal(
+    id int AUTO_INCREMENT PRIMARY Key,
+    fName VARCHAR(50),
+    lName VARCHAR(50),
+    age VARCHAR(50),
+    email VARCHAR(50),
+    pass_word VARCHAR(50),
+    gender VARCHAR(50),
+    phone VARCHAR(50),
+    address VARCHAR(50)
+);
+insert into municipal(fname,lname,age,email,pass_word,gender,phone,address)
+values ("muni","cipal","70","muni@gmail.com","muni123","m","8986566","addr"),
+("muni1","cipal","80","muni1@gmail.com","muni1123","m","78349344","addre"),
+("muni2","cipal","40","muni2@gmail.com","muni2123","m","78349344","addre");
